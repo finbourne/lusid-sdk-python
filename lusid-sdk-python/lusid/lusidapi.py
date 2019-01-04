cf8260318488e565f34487812ddd9a7647be0915
Concourse <concourse@finbourne.com>
2019-01-04 10:57:04 +0000

    Update SDK 0.9.50

--- lusid-sdk-python/lusid/lusidapi.py
+++ lusid-sdk-python/lusid/lusidapi.py
@@ -367,7 +367,7 @@ class LUSIDAPI(object):
         self._client = ServiceClient(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self.api_version = '0.9.46'
+        self.api_version = '0.9.50'
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
