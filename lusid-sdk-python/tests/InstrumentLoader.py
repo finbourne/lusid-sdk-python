20c6eb1354d67564af79bae064b2b6ee982bdbd9
Gus Sekhon <gus.sekhon@finbourne.com>
2019-01-28 09:02:51 +0000

    update to test_get_holdings

---
+++ lusid-sdk-python/tests/InstrumentLoader.py
@@ -0,0 +1,92 @@
+import unittest
+import requests
+import json
+import uuid
+import os
+from datetime import datetime
+import pytz
+import lusid
+import lusid.models as models
+from unittest import TestCase
+from collections import namedtuple
+from msrest.authentication import BasicTokenAuthentication
+from time import sleep
+
+try:
+    # Python 3.x
+    from urllib.request import pathname2url
+except ImportError:
+    # Python 2.7
+    from urllib import pathname2url
+
+
+class InstrumentLoader(object):
+    # any class attributes?
+    inst_list = ["BBG000C6K6G9",
+                 "BBG000C04D57",
+                 "BBG000FV67Q4",
+                 "BBG000BF0KW3",
+                 "BBG000BF4KL1"]
+
+    instrument_response = ""
+
+    def load_instruments(self, test_cls):
+
+        isin_vod = models.InstrumentProperty(test_cls.ISIN_PROPERTY_KEY, models.PropertyValue("GB00BH4HKS39"))
+        sedol_vod = models.InstrumentProperty(test_cls.SEDOL_PROPERTY_KEY, models.PropertyValue("BH4HKS3"))
+
+        instrument_definition1 = models.InstrumentDefinition(name="VODAFONE GROUP PLC",
+                                                             identifiers={test_cls.FIGI_SCHEME: "BBG000C6K6G9",
+                                                                          test_cls.CUSTOM_INTERNAL_SCHEME: "INTERNAL_ID_1"},
+                                                             properties=[isin_vod, sedol_vod])
+
+        isin_barc = models.InstrumentProperty(test_cls.ISIN_PROPERTY_KEY, models.PropertyValue("GB0031348658"))
+        sedol_barc = models.InstrumentProperty(test_cls.SEDOL_PROPERTY_KEY, models.PropertyValue("3134865"))
+
+        instrument_definition2 = models.InstrumentDefinition(name="BARCLAYS PLC",
+                                                             identifiers={test_cls.FIGI_SCHEME: "BBG000C04D57",
+                                                                          test_cls.CUSTOM_INTERNAL_SCHEME: "INTERNAL_ID_2"},
+                                                             properties=[isin_barc, sedol_barc])
+
+        isin_grid = models.InstrumentProperty(test_cls.ISIN_PROPERTY_KEY, models.PropertyValue("GB00BDR05C01"))
+        sedol_grid = models.InstrumentProperty(test_cls.SEDOL_PROPERTY_KEY, models.PropertyValue("BDR05C0"))
+
+        instrument_definition3 = models.InstrumentDefinition(name="NATIONAL GRID PLC",
+                                                             identifiers={test_cls.FIGI_SCHEME: "BBG000FV67Q4",
+                                                                          test_cls.CUSTOM_INTERNAL_SCHEME: "INTERNAL_ID_3"},
+                                                             properties=[isin_grid, sedol_grid])
+
+        isin_sains = models.InstrumentProperty(test_cls.ISIN_PROPERTY_KEY, models.PropertyValue("GB00B019KW72"))
+        sedol_sains = models.InstrumentProperty(test_cls.SEDOL_PROPERTY_KEY, models.PropertyValue("B019KW7"))
+
+        instrument_definition4 = models.InstrumentDefinition(name="SAINSBURY (J) PLC",
+                                                             identifiers={test_cls.FIGI_SCHEME: "BBG000BF0KW3",
+                                                                          test_cls.CUSTOM_INTERNAL_SCHEME: "INTERNAL_ID_4"},
+                                                             properties=[isin_sains, sedol_sains])
+
+        isin_tayl = models.InstrumentProperty(test_cls.ISIN_PROPERTY_KEY, models.PropertyValue("GB0008782301"))
+        sedol_tayl = models.InstrumentProperty(test_cls.SEDOL_PROPERTY_KEY, models.PropertyValue("0878230"))
+
+        instrument_definition5 = models.InstrumentDefinition(name="TAYLOR WIMPEY PLC",
+                                                             identifiers={test_cls.FIGI_SCHEME: "BBG000BF4KL1",
+                                                                          test_cls.CUSTOM_INTERNAL_SCHEME: "INTERNAL_ID_5"},
+                                                             properties=[isin_tayl, sedol_tayl])
+
+        upsert_response = test_cls.client.upsert_instruments(
+            {
+                "correlationId1": instrument_definition1,
+                "correlationId2": instrument_definition2,
+                "correlationId3": instrument_definition3,
+                "correlationId4": instrument_definition4,
+                "correlationId5": instrument_definition5
+            }
+        )
+        assert len(upsert_response.values) == 5
+        self.instrument_response = upsert_response
+        return upsert_response
+
+    @classmethod
+    def tearDownClass(self, test_cls):
+
+        for i in self.inst_list:
+            response = test_cls.client.delete_instrument(test_cls.FIGI_SCHEME, i)
---
+++ lusid-sdk-python/tests/TestDataUtilities.py
@@ -0,0 +1,75 @@
+import unittest
+import requests
+import json
+import uuid
+import os
+from datetime import datetime
+import pytz
+import lusid
+import lusid.models as models
+from unittest import TestCase
+from collections import namedtuple
+from msrest.authentication import BasicTokenAuthentication
+from time import sleep
+
+try:
+    # Python 3.x
+    from urllib.request import pathname2url
+except ImportError:
+    # Python 2.7
+    from urllib import pathname2url
+
+
+class TestDataUtilities(object):
+    # any class attributes?
+    def __init__(self, client):
+        self.client = client
+
+
+    def create_transaction_portfolio(self, scope):
+
+        uuid_gen = uuid.uuid4()
+        effective_date = datetime(2018, 1, 1, tzinfo=pytz.utc)
+
+        # portfolio request
+        original_portfolio_id = "Id-" + str(uuid_gen)
+        request = models.CreateTransactionPortfolioRequest("Portfolio-" + str(uuid_gen),
+                                                           original_portfolio_id,
+                                                           base_currency="GBP",
+                                                           created=effective_date)
+        # create the portfolio
+        portfolio_response = self.client.create_portfolio(scope, request)
+
+        self.assertEqual(portfolio_response.id.code, request.code)
+
+        return portfolio_response.id.code
+
+    def build_transaction_request(self, instrument_id, units, price, currency, trade_date, transaction_type):
+
+        tran_response = models.TransactionRequest(
+                                                        transaction_id=str(uuid.uuid4()),
+                                                        type=transaction_type,
+                                                        instrument_uid=instrument_id,
+                                                        transaction_date=trade_date,
+                                                        settlement_date=trade_date,
+                                                        units=units,
+                                                        transaction_price=models.TransactionPrice(price),
+                                                        total_consideration=models.CurrencyAndAmount(units*price, currency),
+                                                        source="Client")
+
+        return tran_response
+
+    def build_cash_funds_in_transaction_request(self, units, currency, trade_date):
+
+        tran_response = models.TransactionRequest(
+                                                        transaction_id=str(uuid.uuid4()),
+                                                        type="FundsIn",
+                                                        instrument_uid="CCY_" + currency,
+                                                        transaction_date=trade_date,
+                                                        settlement_date=trade_date,
+                                                        units=units,
+                                                        transaction_price=models.TransactionPrice(0),
+                                                        total_consideration=models.CurrencyAndAmount(0, currency),
+                                                        source="Client")
+
+        return tran_response
