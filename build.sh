26dc34b1e34cef2069582552987a64965dad87c7
Michael McGarry <mike.mcgarry@finbourne.com>
2019-04-03 13:09:19 +0100

    Push for travis build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # get the specificed swagger file
 curl -L https://api-am-ci.lusid.com/swagger/v0/swagger.json -o lusid.json
 
-#   build the sdk
+# build the sdk
 docker-compose build && docker-compose up && docker-compose rm -f
