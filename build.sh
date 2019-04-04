bfe1b200a5911bdd77d06ad431e75010a55fa9bc
Michael McGarry <mike.mcgarry@finbourne.com>
2019-04-04 14:50:42 +0100

    openapi-gen Removed files leftover from generation and publish, updated build script to use --build flag instead of a separate command

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 curl -L https://api-am-ci.lusid.com/swagger/v0/swagger.json -o lusid.json
 
 # build the sdk
-docker-compose build && docker-compose up && docker-compose rm -f
+docker-compose up --build && docker-compose rm -f
