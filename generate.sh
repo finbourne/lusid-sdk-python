61f2d2583ef0a1ff683e889c7842d2bf8d3f4bcf
Concourse <concourse@finbourne.com>
2019-04-29 18:24:31 +0000

    Update SDK 0.10.51b1

--- generate.sh
+++ generate.sh
@@ -48,4 +48,3 @@ rm -f $sdk_output_folder/lusid_README.md
 rm -f $sdk_output_folder/test-requirements.txt
 rm -f $sdk_output_folder/tox.ini
 rm -f $sdk_output_folder/README.md
-rm -f $sdk_output_folder/requirements.txt
