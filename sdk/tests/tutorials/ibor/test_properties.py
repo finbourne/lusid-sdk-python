364a288d7fcafca370039bbec0e47608d6843482
Yemmy Chan <yemmy.chan@finbourne.com>
2019-10-31 16:36:23 +0000

    TR-282: Fix API funcs

--- sdk/tests/tutorials/ibor/test_properties.py
+++ sdk/tests/tutorials/ibor/test_properties.py
@@ -71,7 +71,7 @@ class Properties(unittest.TestCase):
 
         # create portfolio
         portfolio_request = self.transaction_portfolios_api.create_portfolio(scope=TestDataUtilities.tutorials_scope,
-                                                                             create_request=create_portfolio_request)
+                                                                             transaction_portfolio=create_portfolio_request)
 
         # get properties for assertions
         portfolio_properties = self.portfolios_api.get_portfolio_properties(scope=TestDataUtilities.tutorials_scope,
@@ -122,7 +122,7 @@ class Properties(unittest.TestCase):
 
         # Create portfolio
         portfolio_result = self.transaction_portfolios_api.create_portfolio(scope=TestDataUtilities.tutorials_scope,
-                                                                            create_request=create_portfolio_request)
+                                                                            transaction_portfolio=create_portfolio_request)
         portfolio_properties = self.portfolios_api.get_portfolio_properties(scope=TestDataUtilities.tutorials_scope,
                                                                             code=portfolio_result.id.code).properties
         metric_property = portfolio_properties[metric_property_definition_result.key]
