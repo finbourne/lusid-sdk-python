8acb500abf742ab6072e8d800f84d986753b4054
Michael McGarry <mike.mcgarry@finbourne.com>
2021-03-04 06:57:14 +0000

    SE-857 Moved the creation of the first access token into the refresh token's logic so that it is able to get a new access token in the instance that a refresh token expired. Also noticed that the refresh token was using the same refresh token over its entire life instead of updating the refresh token on each refresh of the access token. This has also been resolved

--- sdk/lusid/utilities/refreshing_token.py
+++ sdk/lusid/utilities/refreshing_token.py
@@ -4,72 +4,144 @@ import base64
 from datetime import datetime
 from datetime import timedelta
 from collections import UserString
+from urllib.request import pathname2url
 
 
 class RefreshingToken(UserString):
 
-    def __init__(self, token_url, client_id, client_secret, initial_access_token, initial_token_expiry, refresh_token,
-                 expiry_offset=60, proxies=None, certificate_filename=None):
+    def __init__(self, api_configuration, expiry_offset=60, id_provider_response_handler=None):
         """
         Implementation of UserString that will automatically refresh the token value upon expiry
 
-        :param str token_url: token refresh url
-        :param str client_id: OpenID Connect Client ID
-        :param str client_secret: OpenID Connect Client Secret
-        :param str initial_access_token: initial access token
-        :param int initial_token_expiry: number of seconds the initial token is valid for before expiring
-        :param str refresh_token: initial refresh token
+        :param ApiConfiguration api_configuration: The api configuration with all required values
         :param int expiry_offset: number of seconds before token expiry to refresh the token
-        :param dict proxies: dictionary containing proxy schemas
-        :param str certifiate_filename: The path to the client side certificate to use
         """
 
-        token_data = {
-            "expires": datetime.utcnow() + timedelta(seconds=initial_token_expiry),
-            "access_token": initial_access_token
+        self.token_data = {
+            "expires": None,
+            "access_token": None,
+            "refresh_token": None
         }
+        self.expiry_offset = expiry_offset
+        self.api_configuration = api_configuration
+        self.id_provider_response_handler = id_provider_response_handler
+        self.refresh_func = self.get_refresh_token
 
-        def get_refresh_token():
+    def update_token_data(self, id_provider_json):
+        """
+        Updates the token data from a response from the identity provider
 
-            # check if the token has expired and refresh if needed
-            if token_data["expires"] <= datetime.utcnow():
+        :param id_provider_json: The JSON to use to update the token data
+        """
+        self.token_data["access_token"] = id_provider_json["access_token"]
+        # Set the expiry just before the actual expiry to ensure no failed requests
+        delta = timedelta(seconds=id_provider_json.get("expires_in", 3600) - self.expiry_offset)
+        self.token_data["expires"] = datetime.utcnow() + delta
+        self.token_data["refresh_token"] = id_provider_json["refresh_token"]
 
-                encoded_client = base64.b64encode(bytes(f"{client_id}:{client_secret}", 'utf-8'))
+    def get_access_token(self):
+        """
+        Retrieves an access token from the identity provider using the credentials in the provided configuration
 
-                headers = {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": f"Basic {encoded_client.decode('utf-8')}"
-                }
+        :return: The retrieved access token
+        """
 
-                request_body = f"grant_type=refresh_token&scope=openid client groups offline_access&refresh_token={refresh_token}"
+        encoded_password = pathname2url(self.api_configuration.password)
+        encoded_client_id = pathname2url(self.api_configuration.client_id)
+        encoded_client_secret = pathname2url(self.api_configuration.client_secret)
 
-                # request parameters
-                kwargs = {"headers": headers}
-                kwargs["proxies"] = proxies
-                kwargs["verify"] = certificate_filename
+        # Prepare our authentication request
+        token_request_body = f"grant_type=password&username={self.api_configuration.username}" \
+            f"&password={encoded_password}&scope=openid client groups offline_access" \
+            f"&client_id={encoded_client_id}&client_secret={encoded_client_secret}"
 
-                okta_response = requests.post(token_url, data=request_body, **kwargs)
+        headers = {"Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"}
 
-                if okta_response.status_code != 200:
-                    raise Exception(okta_response.json())
+        # extra request args
+        kwargs = {"headers": headers}
 
-                okta_json = okta_response.json()
+        if self.api_configuration.proxy_config is not None:
+            kwargs["proxies"] = self.api_configuration.proxy_config.format_proxy_schema()
 
-                # set the expiry just before the actual expiry to be able to refresh in time
-                delta = timedelta(seconds=okta_json.get("expires_in", 3600) - expiry_offset)
-                token_data["expires"] = datetime.utcnow() + delta
-                token_data["access_token"] = okta_json["access_token"]
+        # use certificate if supplied
+        if self.api_configuration.certificate_filename is not None:
+            kwargs["verify"] = self.api_configuration.certificate_filename
 
-            return token_data["access_token"]
+        # make request to Okta to get an authentication token
+        id_provider_response = requests.post(self.api_configuration.token_url, data=token_request_body, **kwargs)
 
-        self.refresh_func = get_refresh_token
+        if self.id_provider_response_handler is not None:
+            self.id_provider_response_handler(id_provider_response)
+
+        # Ensure that we have a 200 response code
+        if id_provider_response.status_code != 200:
+            raise ValueError(id_provider_response.json())
+
+        # convert the json encoded response to be able to extract the token values
+        id_provider_json = id_provider_response.json()
+
+        self.update_token_data(id_provider_json)
+
+        return self.token_data["access_token"]
+
+    def get_refresh_token(self):
+        """
+        Retrieves an access token from the identity provider using the refresh token
+
+        :return: The retrieved access token
+        """
+
+        # If any data is missing to use a refresh token e.g. on first try, get an access token using credentials
+        if self.token_data["access_token"] is None or self.token_data["expires"] is None or self.token_data["refresh_token"] is None:
+            return self.get_access_token()
+
+        # check if the token has expired and refresh if needed
+        if self.token_data["expires"] <= datetime.utcnow():
+
+            encoded_client = base64.b64encode(bytes(f"{self.api_configuration.client_id}:{self.api_configuration.client_secret}", 'utf-8'))
+
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": f"Basic {encoded_client.decode('utf-8')}"
+            }
+
+            request_body = f"grant_type=refresh_token&scope=openid client groups offline_access&refresh_token={self.token_data['refresh_token']}"
+
+            # request parameters
+            kwargs = {"headers": headers}
+
+            if self.api_configuration.proxy_config is not None:
+                kwargs["proxies"] = self.api_configuration.proxy_config.format_proxy_schema()
+
+            if self.api_configuration.certificate_filename is not None:
+                kwargs["verify"] = self.api_configuration.certificate_filename
+
+            id_provider_response = requests.post(self.api_configuration.token_url, data=request_body, **kwargs)
+
+            # Refresh token may be expired, if so, get new request token
+            if id_provider_response.status_code == 400 and 'refresh token is invalid or expired' \
+                    in id_provider_response.json()['error_description']:
+                return self.get_access_token()
+            elif id_provider_response.status_code != 200:
+                raise ValueError(id_provider_response.json())
+
+            id_provider_json = id_provider_response.json()
+
+            self.update_token_data(id_provider_json)
+
+        return self.token_data["access_token"]
 
     def __getattribute__(self, item):
 
-        token = object.__getattribute__(self, "refresh_func")()
-
         # return the value of the string
         if item == "data":
-            return token
+            return object.__getattribute__(self, "refresh_func")()
 
-        return token.__getattribute__(item)
+        # get the attribute on the string value, not the RefreshingToken class itself, used for UserString base methods
+        # such as string concatenation, if this is missing the whole RefreshingToken class gets created again with the
+        # concatenated string as the input
+        if item == "__class__":
+            return object.__getattribute__(self, "refresh_func")().__getattribute__(item)
+
+        # used to get .self attributes on the RefreshingToken
+        return object.__getattribute__(self, item)
