6f717b2fa12fe4a921383002732437a451510b1e
Concourse <concourse@finbourne.com>
2021-08-29 13:56:14 +0000

    Update SDK 0.11.3439 [rebase]

--- sdk/lusid/configuration.py
+++ sdk/lusid/configuration.py
@@ -98,7 +98,7 @@ class Configuration(object):
                  ):
         """Constructor
         """
-        self._base_path = "http://local-unit-test-server.lusid.com:62243" if host is None else host
+        self._base_path = "https://fbn-prd.lusid.com/api" if host is None else host
         """Default Base url
         """
         self.server_index = 0 if server_index is None and host is None else server_index
@@ -403,7 +403,7 @@ class Configuration(object):
         """
         return [
             {
-                'url': "http://local-unit-test-server.lusid.com:62243",
+                'url': "https://fbn-prd.lusid.com/api",
                 'description': "No description provided",
             }
         ]
