11c9214a2e98e1d75e204b9f2a492dea95ad968c
mb-a <44067109+mb-a@users.noreply.github.com>
2021-09-07 12:41:53 +0100

    Ensure KeepAlive is used on Linux (may break JupyterHub) (#84)

--- sdk/lusid/tcp/tcp_keep_alive_probes.py
+++ sdk/lusid/tcp/tcp_keep_alive_probes.py
@@ -39,10 +39,11 @@ class TCPKeepAliveValidationMethods:
         platform = sys.platform
         # TCP Keep Alive Probes for Linux
         if platform == 'linux' and hasattr(socket, "TCP_KEEPIDLE") and hasattr(socket, "TCP_KEEPINTVL") and hasattr(socket, "TCP_KEEPCNT"):
-            # Temporarily commented out due to issues running on JupyterHub
-            # conn.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, TCP_KEEP_IDLE)
-            # conn.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, TCP_KEEPALIVE_INTERVAL)
-            # conn.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, TCP_KEEP_CNT)
+            # This may break JupyterHub
+            conn.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            conn.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, TCP_KEEP_IDLE)
+            conn.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, TCP_KEEPALIVE_INTERVAL)
+            conn.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, TCP_KEEP_CNT)
             pass
 
         # TCP Keep Alive Probes for Windows OS
