a0efe19d7db8100dbc827394cc306e9ba7c26735
Omar-Oz <81164562+Omar-Oz@users.noreply.github.com>
2021-11-30 20:15:01 +0000

    Update setup.py (#90)
    Update setup.py to match requirements.txt

--- sdk/setup.py
+++ sdk/setup.py
@@ -37,7 +37,8 @@ REQUIRES = [
     "certifi >= 14.05.14",
     "python-dateutil >= 2.5.3",
     "urllib3 >= 1.15.1",
-    "requests >= 2.21.0"
+    "requests >= 2.21.0",
+    "simplejson >= 3.17.3"
 ]
 
 version = {}
