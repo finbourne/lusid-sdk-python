d08f9c08b3d853ae0e227625a571c1c811bea432
Jeremy Barber <jeremy.barber@finbourne.com>
2022-08-17 15:32:28 +0100

    CTECH-1152 Removed the tests, update the Dockerfile and readme accordingly

--- sdk/tests/utilities/__init__.py
+++
@@ -1,7 +0,0 @@
-from utilities.credentials_source import CredentialsSource
-from utilities.instrument_loader import InstrumentLoader
-from utilities.test_data_utilities import TestDataUtilities
-from utilities.token_utilities import TokenUtilities
-from utilities.temp_file_manager import TempFileManager
-from utilities.mock_api_response import MockApiResponse
-from utilities.id_generator import IdGenerator
