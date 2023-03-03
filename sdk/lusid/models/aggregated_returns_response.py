50f2eccd8c628b17a6a131aaca1b47c0feb41fe0
Concourse <concourse@finbourne.com>
2023-03-03 22:34:15 +0000

    Update SDK 0.11.5300 [rebase]

--- sdk/lusid/models/aggregated_returns_response.py
+++ sdk/lusid/models/aggregated_returns_response.py
@@ -63,7 +63,7 @@ class AggregatedReturnsResponse(object):
         :type href: str
         :param results:  Aggregated returns grouped by ReturnId
         :type results: dict(str, list[AggregatedReturn])
-        :param links:  Collection of links.
+        :param links: 
         :type links: list[lusid.Link]
 
         """  # noqa: E501
@@ -130,7 +130,6 @@ class AggregatedReturnsResponse(object):
     def links(self):
         """Gets the links of this AggregatedReturnsResponse.  # noqa: E501
 
-        Collection of links.  # noqa: E501
 
         :return: The links of this AggregatedReturnsResponse.  # noqa: E501
         :rtype: list[lusid.Link]
@@ -141,7 +140,6 @@ class AggregatedReturnsResponse(object):
     def links(self, links):
         """Sets the links of this AggregatedReturnsResponse.
 
-        Collection of links.  # noqa: E501
 
         :param links: The links of this AggregatedReturnsResponse.  # noqa: E501
         :type links: list[lusid.Link]
