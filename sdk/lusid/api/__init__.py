9c0e23ca58f199428f34ec91591572183d0d278c
Concourse <concourse@finbourne.com>
2023-04-05 15:54:57 +0000

    Update SDK 1.0.53 [rebase]

--- sdk/lusid/api/__init__.py
+++ sdk/lusid/api/__init__.py
@@ -24,7 +24,6 @@ from lusid.api.legal_entities_api import LegalEntitiesApi
 from lusid.api.orders_api import OrdersApi
 from lusid.api.participations_api import ParticipationsApi
 from lusid.api.persons_api import PersonsApi
-from lusid.api.placements_api import PlacementsApi
 from lusid.api.portfolio_groups_api import PortfolioGroupsApi
 from lusid.api.portfolios_api import PortfoliosApi
 from lusid.api.property_definitions_api import PropertyDefinitionsApi
