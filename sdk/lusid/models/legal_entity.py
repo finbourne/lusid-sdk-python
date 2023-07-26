8e77aebb721a7eb066e738394a8f569e05a2243a
Concourse <concourse@finbourne.com>
2023-07-26 19:43:49 +0000

    Update SDK 1.0.384 [rebase]

--- sdk/lusid/models/legal_entity.py
+++ sdk/lusid/models/legal_entity.py
@@ -98,7 +98,7 @@ class LegalEntity(object):
         :type counterparty_risk_information: lusid.CounterpartyRiskInformation
         :param version: 
         :type version: lusid.Version
-        :param links:  Collection of links.
+        :param links: 
         :type links: list[lusid.Link]
 
         """  # noqa: E501
@@ -338,7 +338,6 @@ class LegalEntity(object):
     def links(self):
         """Gets the links of this LegalEntity.  # noqa: E501
 
-        Collection of links.  # noqa: E501
 
         :return: The links of this LegalEntity.  # noqa: E501
         :rtype: list[lusid.Link]
@@ -349,7 +348,6 @@ class LegalEntity(object):
     def links(self, links):
         """Sets the links of this LegalEntity.
 
-        Collection of links.  # noqa: E501
 
         :param links: The links of this LegalEntity.  # noqa: E501
         :type links: list[lusid.Link]
