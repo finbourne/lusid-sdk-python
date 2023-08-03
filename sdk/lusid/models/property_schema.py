711fad8215d331ce5ffa3c08696fed154a998602
Concourse <concourse@finbourne.com>
2023-08-03 21:16:40 +0000

    Update SDK 1.0.411 [rebase]

--- sdk/lusid/models/property_schema.py
+++ sdk/lusid/models/property_schema.py
@@ -63,7 +63,7 @@ class PropertySchema(object):
         :type href: str
         :param values: 
         :type values: dict[str, lusid.FieldSchema]
-        :param links:  Collection of links.
+        :param links: 
         :type links: list[lusid.Link]
 
         """  # noqa: E501
@@ -126,7 +126,6 @@ class PropertySchema(object):
     def links(self):
         """Gets the links of this PropertySchema.  # noqa: E501
 
-        Collection of links.  # noqa: E501
 
         :return: The links of this PropertySchema.  # noqa: E501
         :rtype: list[lusid.Link]
@@ -137,7 +136,6 @@ class PropertySchema(object):
     def links(self, links):
         """Sets the links of this PropertySchema.
 
-        Collection of links.  # noqa: E501
 
         :param links: The links of this PropertySchema.  # noqa: E501
         :type links: list[lusid.Link]
