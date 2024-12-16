6fa6991e386c3655062bd5602b136b3f003c7d20
Concourse <concourse@finbourne.com>
2024-12-16 17:11:55 +0000

    Updates for v2.1.635

--- sdk/lusid/configuration.py
+++ sdk/lusid/configuration.py
@@ -445,7 +445,7 @@ class Configuration:
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 0.11.7207\n"\
+               "Version of the API: 0.11.7209\n"\
                "SDK Package Version: {package_version}".\
                format(env=sys.platform, pyversion=sys.version, package_version=package_version)
 
