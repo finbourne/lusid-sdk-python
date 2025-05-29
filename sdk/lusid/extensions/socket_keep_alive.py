c8d7b6e949936bcba09353f0961fe948232bb6ac
Concourse <concourse@finbourne.com>
2025-05-29 15:17:27 +0000

    Updates for v2.1.808

--- sdk/lusid/extensions/socket_keep_alive.py
+++ sdk/lusid/extensions/socket_keep_alive.py
@@ -2,6 +2,7 @@ from typing import Sequence
 import socket
 import logging
 from urllib3.connection import HTTPConnection
+import platform
 
 __logger__ = logging.getLogger(__name__)
 
@@ -24,29 +25,34 @@ def keep_alive_socket_options() -> Sequence:
     Sequence
         Set of socket options.
     """
-    try:
-        # linux and some windows runtimes
-        return HTTPConnection.default_socket_options + [
-            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
-            (socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, TCP_KEEP_IDLE),
-            (socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, TCP_KEEPALIVE_INTERVAL),
-            (socket.IPPROTO_TCP, socket.TCP_KEEPCNT, TCP_KEEP_CNT),
-        ]
-    except AttributeError:
-        pass
-    try:
-        # darwin
-        return HTTPConnection.default_socket_options + [
-            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
-            (socket.IPPROTO_TCP, TCP_KEEPALIVE, TCP_KEEPALIVE_INTERVAL),
-        ]
-    except AttributeError:
-        pass
-    try:
-        # windows
-        return HTTPConnection.default_socket_options + [
+    base_options = HTTPConnection.default_socket_options
+    system = platform.system().title()
+
+    if system == "Windows":
+        # Only SO_KEEPALIVE is supported and safe on Windows Server 2016
+        __logger__.info("Setting socket settings for Windows")
+        return base_options + [
             (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
         ]
-    except AttributeError:
-        __logger__.exception("Unable to set TCP Keep-alive socket options")
-        raise
+    elif system == "Darwin":
+        # macOS specific
+        __logger__.info("Setting socket settings for Darwin (macOS)")
+        TCP_KEEPALIVE = 0x10  # from <netinet/tcp.h>
+        return base_options + [
+            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
+            (socket.IPPROTO_TCP, TCP_KEEPALIVE, 60)
+        ]
+    elif system == "Linux":
+        # Safe on Linux
+        __logger__.info("Setting socket settings for Linux")
+        return base_options + [
+            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
+            (socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 60),
+            (socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 60),
+            (socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 3)
+        ]
+    else:
+        __logger__.warning("Unsupported platform for TCP keep-alive fine-tuning. Using basic SO_KEEPALIVE.")
+        return base_options + [
+            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        ]
